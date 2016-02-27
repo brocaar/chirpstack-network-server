bb8e190ca3bcc0f13bbdd86570747aaa8780a050
Orne Brocaar <info@brocaar.com>
2016-02-27 16:20:21 +0100

    Fields should not be nullable.

--- migrations/0001_initial_up.sql
+++ migrations/0001_initial_up.sql
@@ -1,22 +1,22 @@
 create table application (
 	app_eui bytea primary key,
-	name character varying (100)
+	name character varying (100) not null
 );
 
 create table node (
 	dev_eui bytea primary key,
-	app_eui bytea references application,
-	app_key bytea,
+	app_eui bytea references application not null,
+	app_key bytea not null,
 	used_dev_nonces bytea[]
 );
 
 create table node_abp (
 	dev_addr bytea primary key,
-	dev_eui bytea references node,
-	app_s_key bytea,
-	nwk_s_key bytea,
-	fcnt_up integer,
-	fcnt_down integer
+	dev_eui bytea references node not null,
+	app_s_key bytea not null,
+	nwk_s_key bytea not null,
+	fcnt_up integer not null,
+	fcnt_down integer not null
 );
 
 create index node_app_eui on node (app_eui);
