283feea5cb598c747a502654407bd9095e44234d
Orne Brocaar <info@brocaar.com>
2016-03-06 10:16:15 +0100

    Add functions for getting AppNonce and NwkSKey and AppSKey.

--- node_session.go
+++ node_session.go
@@ -2,9 +2,11 @@ package loraserver
 
 import (
 	"bytes"
+	"crypto/aes"
 	"crypto/rand"
 	"encoding/gob"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/brocaar/lorawan"
@@ -163,4 +165,52 @@ func GetRandomDevAddr(p *redis.Pool, netID lorawan.NetID) (lorawan.DevAddr, erro
 		return lorawan.DevAddr{}, errors.New("DevAddr already exists")
 	}
 	return d, nil
+}
+
+// GetAppNonce returns a random application nonce (used for OTAA).
+func GetAppNonce() ([3]byte, error) {
+	var b [3]byte
+	if _, err := rand.Read(b[:]); err != nil {
+		return b, err
+	}
+	return b, nil
+}
+
+// GetNwkSKey returns the network session key.
+func GetNwkSKey(appkey lorawan.AES128Key, netID lorawan.NetID, appNonce [3]byte, devNonce [2]byte) (lorawan.AES128Key, error) {
+	return getSKey(0x01, appkey, netID, appNonce, devNonce)
+}
+
+// GetAppSKey returns the application session key.
+func GetAppSKey(appkey lorawan.AES128Key, netID lorawan.NetID, appNonce [3]byte, devNonce [2]byte) (lorawan.AES128Key, error) {
+	return getSKey(0x02, appkey, netID, appNonce, devNonce)
+}
+
+func getSKey(typ byte, appkey lorawan.AES128Key, netID lorawan.NetID, appNonce [3]byte, devNonce [2]byte) (lorawan.AES128Key, error) {
+	var key lorawan.AES128Key
+	b := make([]byte, 0, 16)
+	b = append(b, typ)
+
+	// little endian
+	for i := len(appNonce) - 1; i >= 0; i-- {
+		b = append(b, appNonce[i])
+	}
+	for i := len(netID) - 1; i >= 0; i-- {
+		b = append(b, netID[i])
+	}
+	for i := len(devNonce) - 1; i >= 0; i-- {
+		b = append(b, devNonce[i])
+	}
+	pad := make([]byte, 7)
+	b = append(b, pad...)
+
+	block, err := aes.NewCipher(appkey[:])
+	if err != nil {
+		return key, err
+	}
+	if block.BlockSize() != len(b) {
+		return key, fmt.Errorf("block-size of %d bytes is expected", len(b))
+	}
+	block.Encrypt(key[:], b)
+	return key, nil
 }
