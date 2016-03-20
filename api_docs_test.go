45ca49f287ef48fad4fbcd5b07d61563690d0b95
Orne Brocaar <info@brocaar.com>
2016-03-20 12:14:07 +0100

    Add tests for api documentation.

---
+++ api_docs_test.go
@@ -0,0 +1,38 @@
+package loraserver
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGetRPCServicesDoc(t *testing.T) {
+	Convey("Given a RPC service", t, func() {
+		api := NewApplicationAPI(Context{})
+
+		Convey("Then all expected strings are in the returned JSON", func() {
+			doc, err := getRPCServicesDoc(api)
+			So(err, ShouldBeNil)
+
+			Convey("Then the doc has an Application entry", func() {
+				app, ok := doc["Application"]
+				So(ok, ShouldBeTrue)
+
+				Convey("Which has a Create method", func() {
+					meth, ok := app.Methods["Create"]
+					So(ok, ShouldBeTrue)
+
+					Convey("Which has the expected argument and reply", func() {
+						So(meth.ArgumentTypeName, ShouldEqual, "Application")
+						So(meth.ArgumentPkgPath, ShouldEqual, "github.com/brocaar/loraserver")
+						So(meth.ArgumentJSON, ShouldContainSubstring, "appEUI")
+						So(meth.ArgumentJSON, ShouldContainSubstring, "Application.Create")
+						So(meth.ReplyTypeName, ShouldEqual, "EUI64")
+						So(meth.ReplyPkgPath, ShouldEqual, "github.com/brocaar/lorawan")
+						So(meth.ReplyJSON, ShouldContainSubstring, "0000000000000000")
+					})
+				})
+			})
+		})
+	})
+}
