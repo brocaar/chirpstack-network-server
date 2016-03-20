0fd5421d1cfa29deedb86cb36379c2617da6d0dc
Orne Brocaar <info@brocaar.com>
2016-03-20 13:04:11 +0100

    Do not close the connection now we get a random id.

--- application/mqttpubsub/backend_test.go
+++ application/mqttpubsub/backend_test.go
@@ -19,7 +19,6 @@ func TestBackend(t *testing.T) {
 		token := c.Connect()
 		token.Wait()
 		So(token.Error(), ShouldBeNil)
-		defer c.Disconnect(250)
 
 		Convey("Given a new Backend", func() {
 			backend, err := NewBackend(conf.Server, conf.Username, conf.Password)
