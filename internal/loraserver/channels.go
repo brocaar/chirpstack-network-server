e5113bc7862e0ce7f9d14be6b94a19afb7ac6505
Orne Brocaar <info@brocaar.com>
2016-05-08 13:19:45 +0200

    Implement Channel CRUD methods.

--- internal/loraserver/channels.go
+++ internal/loraserver/channels.go
@@ -83,6 +83,85 @@ func deleteChannelSet(db *sqlx.DB, id int64) error {
 	return nil
 }
 
+// createChannel creates the given channel.
+func createChannel(db *sqlx.DB, c *models.Channel) error {
+	err := db.Get(&c.ID, "insert into channel (channel_set_id, channel, frequency) values ($1, $2, $3) returning id",
+		c.ChannelSetID,
+		c.Channel,
+		c.Frequency,
+	)
+	if err != nil {
+		return fmt.Errorf("create channel %d for channel-set %d error: %s", c.Channel, c.ChannelSetID, err)
+	}
+	log.WithFields(log.Fields{
+		"channel_set_id": c.ChannelSetID,
+		"channel":        c.Channel,
+		"id":             c.ID,
+	}).Info("channel created")
+	return nil
+}
+
+// updateChannel updates the given Channel.
+func updateChannel(db *sqlx.DB, c models.Channel) error {
+	res, err := db.Exec("update channel set channel_set_id = $1, channel = $2, frequency = $3 where id = $4",
+		c.ChannelSetID,
+		c.Channel,
+		c.Frequency,
+		c.ID,
+	)
+	if err != nil {
+		return fmt.Errorf("update channel %d error: %s", c.ID, err)
+	}
+	ra, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if ra == 0 {
+		return fmt.Errorf("channel %d does not exist", c.ID)
+	}
+	log.WithField("id", c.ID).Info("channel updated")
+	return nil
+}
+
+// deleteChannel deletes the Channel matching the given id.
+func deleteChannel(db *sqlx.DB, id int64) error {
+	res, err := db.Exec("delete from channel where id = $1",
+		id,
+	)
+	if err != nil {
+		return fmt.Errorf("delete channel %d error: %s", id, err)
+	}
+	ra, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if ra == 0 {
+		return fmt.Errorf("channel %d does not exist", id)
+	}
+	log.WithField("id", id).Info("channel deleted")
+	return nil
+}
+
+// getChannel returns the Channel matching the given id.
+func getChannel(db *sqlx.DB, id int64) (models.Channel, error) {
+	var channel models.Channel
+	err := db.Get(&channel, "select * from channel where id = $1", id)
+	if err != nil {
+		return channel, fmt.Errorf("get channel %d error: %s", id, err)
+	}
+	return channel, nil
+}
+
+// getChannels returns the Channels for the given ChannelSet id.
+func getChannelsForChannelSet(db *sqlx.DB, channelSetID int64) ([]models.Channel, error) {
+	var channels []models.Channel
+	err := db.Select(&channels, "select * from channel where channel_set_id = $1 order by channel", channelSetID)
+	if err != nil {
+		return nil, fmt.Errorf("get channels for channel-set %d error: %s", channelSetID, err)
+	}
+	return channels, nil
+}
+
 // ChannelSetAPI struct exports the channel-set related functions.
 type ChannelSetAPI struct {
 	ctx Context
@@ -134,4 +213,57 @@ func (a *ChannelSetAPI) Delete(id int64, deletedID *int64) error {
 	}
 	*deletedID = id
 	return nil
+}
+
+// ChannelAPI struct exports the channel related functions.
+type ChannelAPI struct {
+	ctx Context
+}
+
+// NewChannelAPI creates a new ChannelAPI.
+func NewChannelAPI(ctx Context) *ChannelAPI {
+	return &ChannelAPI{
+		ctx: ctx,
+	}
+}
+
+// Create creates the given Channel.
+func (a *ChannelAPI) Create(c models.Channel, id *int64) error {
+	if err := createChannel(a.ctx.DB, &c); err != nil {
+		return err
+	}
+	*id = c.ID
+	return nil
+}
+
+// Update updates the given Channel.
+func (a *ChannelAPI) Update(c models.Channel, id *int64) error {
+	if err := updateChannel(a.ctx.DB, c); err != nil {
+		return err
+	}
+	*id = c.ID
+	return nil
+}
+
+// Delete deletes the Channel matching the given id.
+func (a *ChannelAPI) Delete(id int64, deletedID *int64) error {
+	if err := deleteChannel(a.ctx.DB, id); err != nil {
+		return err
+	}
+	*deletedID = id
+	return nil
+}
+
+// GetForChannelSet returns the channels for the given ChannelSet id.
+func (a *ChannelAPI) GetForChannelSet(id int64, channels *[]models.Channel) error {
+	var err error
+	*channels, err = getChannelsForChannelSet(a.ctx.DB, id)
+	return err
+}
+
+// Get returns the Channel matching the given id.
+func (a *ChannelAPI) Get(id int64, channel *models.Channel) error {
+	var err error
+	*channel, err = getChannel(a.ctx.DB, id)
+	return err
 }
--- internal/loraserver/channels_test.go
+++ internal/loraserver/channels_test.go
@@ -56,3 +56,68 @@ func TestChannelSetAPI(t *testing.T) {
 		})
 	})
 }
+
+func TestChannelAPI(t *testing.T) {
+	conf := getConfig()
+
+	Convey("Given a clean database and an API instance", t, func() {
+		db, err := OpenDatabase(conf.PostgresDSN)
+		So(err, ShouldBeNil)
+		mustResetDB(db)
+
+		ctx := Context{
+			DB: db,
+		}
+		api := NewChannelAPI(ctx)
+
+		Convey("Given a channel-set in the database", func() {
+			cs := models.ChannelSet{
+				Name: "test set",
+			}
+			So(createChannelSet(db, &cs), ShouldBeNil)
+
+			c1 := models.Channel{
+				ChannelSetID: cs.ID,
+				Channel:      0,
+				Frequency:    868100000,
+			}
+			c2 := models.Channel{
+				ChannelSetID: cs.ID,
+				Channel:      1,
+				Frequency:    868200000,
+			}
+
+			Convey("When calling Create", func() {
+				So(api.Create(c1, &c1.ID), ShouldBeNil)
+
+				Convey("Then the channel has been created", func() {
+					var c models.Channel
+					So(api.Get(c1.ID, &c), ShouldBeNil)
+					So(c, ShouldResemble, c1)
+
+					Convey("Then the channel can be updated", func() {
+						c1.Frequency = 868300000
+						So(api.Update(c1, &c1.ID), ShouldBeNil)
+						So(api.Get(c1.ID, &c), ShouldBeNil)
+						So(c, ShouldResemble, c1)
+					})
+
+					Convey("Then the channel can be deleted", func() {
+						So(api.Delete(c1.ID, &c1.ID), ShouldBeNil)
+						So(api.Get(c1.ID, &c), ShouldNotBeNil)
+					})
+
+					Convey("When creating a second channel", func() {
+						So(api.Create(c2, &c2.ID), ShouldBeNil)
+
+						Convey("Then GetForChannelSet returns two channels", func() {
+							var channels []models.Channel
+							So(api.GetForChannelSet(cs.ID, &channels), ShouldBeNil)
+							So(channels, ShouldHaveLength, 2)
+						})
+					})
+				})
+			})
+		})
+	})
+}
