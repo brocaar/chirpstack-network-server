d99d784eb6d566f782cf7a17a11f4bef3893ac0c
Orne Brocaar <info@brocaar.com>
2016-08-22 17:27:21 +0200

    Move gateway models to api/gw.

---
+++ api/gw/gw.go
@@ -0,0 +1,61 @@
+package gw
+
+import (
+	"time"
+
+	"github.com/brocaar/lorawan"
+	"github.com/brocaar/lorawan/band"
+)
+
+// RXPacket contains the PHYPayload received from GatewayMAC.
+type RXPacket struct {
+	RXInfo     RXInfo             `json:"rxInfo"`
+	PHYPayload lorawan.PHYPayload `json:"phyPayload"`
+}
+
+// RXInfo contains the RX information.
+type RXInfo struct {
+	MAC       lorawan.EUI64 `json:"mac"`       // MAC address of the gateway
+	Time      time.Time     `json:"time"`      // receive time
+	Timestamp uint32        `json:"timestamp"` // gateway internal receive timestamp with microsecond precision, will rollover every ~ 72 minutes
+	Frequency int           `json:"frequency"` // frequency in Hz
+	Channel   int           `json:"channel"`   // concentrator IF channel used for RX
+	RFChain   int           `json:"rfChain"`   // RF chain used for RX
+	CRCStatus int           `json:"crcStatus"` // 1 = OK, -1 = fail, 0 = no CRC
+	CodeRate  string        `json:"codeRate"`  // ECC code rate
+	RSSI      int           `json:"rssi"`      // RSSI in dBm
+	LoRaSNR   float64       `json:"loRaSNR"`   // LoRa signal-to-noise ratio in dB
+	Size      int           `json:"size"`      // packet payload size
+	DataRate  band.DataRate `json:"dataRate"`  // RX datarate (either LoRa or FSK)
+}
+
+// TXPacket contains the PHYPayload which should be send to the
+// gateway.
+type TXPacket struct {
+	TXInfo     TXInfo             `json:"txInfo"`
+	PHYPayload lorawan.PHYPayload `json:"phyPayload"`
+}
+
+// TXInfo contains the information used for TX.
+type TXInfo struct {
+	MAC         lorawan.EUI64 `json:"mac"`         // MAC address of the gateway
+	Immediately bool          `json:"immediately"` // send the packet immediately (ignore Time)
+	Timestamp   uint32        `json:"timestamp"`   // gateway internal receive timestamp with microsecond precision, will rollover every ~ 72 minutes
+	Frequency   int           `json:"frequency"`   // frequency in Hz
+	Power       int           `json:"power"`       // TX power to use in dBm
+	DataRate    band.DataRate `json:"dataRate"`    // TX datarate (either LoRa or FSK)
+	CodeRate    string        `json:"codeRate"`    // ECC code rate
+}
+
+// GatewayStatsPacket contains the information of a gateway.
+type GatewayStatsPacket struct {
+	MAC                 lorawan.EUI64          `json:"mac"`
+	Time                time.Time              `json:"time"`
+	Latitude            float64                `json:"latitude"`
+	Longitude           float64                `json:"longitude"`
+	Altitude            float64                `json:"altitude"`
+	RXPacketsReceived   int                    `json:"rxPacketsReceived"`
+	RXPacketsReceivedOK int                    `json:"rxPacketsReceivedOK"`
+	TXPacketsEmitted    int                    `json:"txPacketsEmitted"`
+	CustomData          map[string]interface{} `json:"customData"` // custom fields defined by alternative packet_forwarder versions (e.g. TTN sends platform, contactEmail, and description)
+}
