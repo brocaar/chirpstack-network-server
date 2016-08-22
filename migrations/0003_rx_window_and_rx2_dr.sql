1585a346ebec229bcd60d90239cfdf9bbab50ac1
Orne Brocaar <info@brocaar.com>
2016-08-22 17:32:12 +0200

    Remove migrations.

--- migrations/0003_rx_window_and_rx2_dr.sql
+++
@@ -1,9 +0,0 @@
--- +migrate Up
-alter table node
-	add column rx_window int2 not null default 0,
-	add column rx2_dr int2 not null default 0;
-
--- +migrate Down
-alter table node
-	drop column rx_window,
-	drop column rx2_dr;
