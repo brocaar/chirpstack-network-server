76c585fb12d589805fbbfb9a6f2317b648d511bf
Orne Brocaar <info@brocaar.com>
2017-02-15 15:01:07 +0100

    Implement subscribing to gw stats.

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -5,7 +5,8 @@ import "github.com/brocaar/loraserver/api/gw"
 // Gateway is the interface of a gateway backend.
 // A gateway backend is responsible for the communication with the gateway.
 type Gateway interface {
-	SendTXPacket(gw.TXPacket) error // send the given packet to the gateway
-	RXPacketChan() chan gw.RXPacket // channel containing the received packets
-	Close() error                   // close the gateway backend.
+	SendTXPacket(gw.TXPacket) error              // send the given packet to the gateway
+	RXPacketChan() chan gw.RXPacket              // channel containing the received packets
+	StatsPacketChan() chan gw.GatewayStatsPacket // channel containing the received gateway stats
+	Close() error                                // close the gateway backend.
 }
