d88aed08eb710f4027a94803190748ec6c5c8fc9
Orne Brocaar <info@brocaar.com>
2018-01-23 15:20:56 +0100

    Use grpc SetLoggerV2.

--- cmd/loraserver/main.go
+++ cmd/loraserver/main.go
@@ -44,8 +44,49 @@ import (
 	"github.com/brocaar/lorawan/band"
 )
 
+// grpcLogger implements a wrapper around the logrus Logger to make it
+// compatible with the grpc LoggerV2. It seems that V is not (always)
+// called, therefore the Info* methods are overridden as we want to
+// log these as debug info.
+type grpcLogger struct {
+	*log.Logger
+}
+
+func (gl *grpcLogger) V(l int) bool {
+	level, ok := map[log.Level]int{
+		log.DebugLevel: 0,
+		log.InfoLevel:  1,
+		log.WarnLevel:  2,
+		log.ErrorLevel: 3,
+		log.FatalLevel: 4,
+	}[log.GetLevel()]
+	if !ok {
+		return false
+	}
+
+	return l >= level
+}
+
+func (gl *grpcLogger) Info(args ...interface{}) {
+	if log.GetLevel() == log.DebugLevel {
+		log.Debug(args...)
+	}
+}
+
+func (gl *grpcLogger) Infoln(args ...interface{}) {
+	if log.GetLevel() == log.DebugLevel {
+		log.Debug(args...)
+	}
+}
+
+func (gl *grpcLogger) Infof(format string, args ...interface{}) {
+	if log.GetLevel() == log.DebugLevel {
+		log.Debugf(format, args...)
+	}
+}
+
 func init() {
-	grpclog.SetLogger(log.StandardLogger())
+	grpclog.SetLoggerV2(&grpcLogger{log.StandardLogger()})
 }
 
 var version string // set by the compiler
