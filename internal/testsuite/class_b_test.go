73b73db25741bcf3614fd53a0222e0c48bf7bc73
Orne Brocaar <info@brocaar.com>
2018-09-25 16:57:27 +0200

    Refactor Class-B downlink tests.

--- internal/testsuite/class_b_test.go
+++ internal/testsuite/class_b_test.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/brocaar/loraserver/api/gw"
 	"github.com/brocaar/loraserver/internal/config"
-	"github.com/brocaar/loraserver/internal/downlink"
 	"github.com/brocaar/loraserver/internal/gps"
 	"github.com/brocaar/loraserver/internal/helpers"
 	"github.com/brocaar/loraserver/internal/storage"
@@ -20,17 +19,11 @@ import (
 )
 
 type ClassBTestSuite struct {
-	suite.Suite
-	test.DatabaseTestSuiteBase
-
-	Device  storage.Device
-	Gateway storage.Gateway
+	IntegrationTestSuite
 }
 
 func (ts *ClassBTestSuite) SetupSuite() {
 	ts.DatabaseTestSuiteBase.SetupSuite()
-
-	assert := require.New(ts.T())
 
 	asClient := test.NewApplicationClient()
 	config.C.ApplicationServer.Pool = test.NewApplicationServerPool(asClient)
@@ -39,47 +32,28 @@ func (ts *ClassBTestSuite) SetupSuite() {
 	config.C.NetworkServer.NetworkSettings.ClassB.PingSlotDR = 2
 	config.C.NetworkServer.NetworkSettings.ClassB.PingSlotFrequency = 868300000
 
-	ts.Gateway = storage.Gateway{
+	ts.CreateGateway(storage.Gateway{
 		GatewayID: [8]byte{1, 2, 3, 4, 5, 6, 7, 8},
 		Location: storage.GPSPoint{
 			Latitude:  1.1234,
 			Longitude: 1.1235,
 		},
 		Altitude: 10.5,
-	}
-	assert.NoError(storage.CreateGateway(ts.DB(), &ts.Gateway))
+	})
 
-	// service-profile
-	sp := storage.ServiceProfile{
+	ts.CreateServiceProfile(storage.ServiceProfile{
 		AddGWMetadata: true,
-	}
-	assert.NoError(storage.CreateServiceProfile(ts.DB(), &sp))
+	})
 
 	// device-profile
-	dp := storage.DeviceProfile{
+	ts.CreateDeviceProfile(storage.DeviceProfile{
 		SupportsClassB: true,
-	}
-	assert.NoError(storage.CreateDeviceProfile(ts.DB(), &dp))
-
-	// routing-profile
-	rp := storage.RoutingProfile{}
-	assert.NoError(storage.CreateRoutingProfile(ts.DB(), &rp))
+	})
 
 	// device
-	ts.Device = storage.Device{
-		ServiceProfileID: sp.ID,
-		DeviceProfileID:  dp.ID,
-		RoutingProfileID: rp.ID,
-		DevEUI:           lorawan.EUI64{1, 2, 3, 4, 5, 6, 7, 8},
-	}
-	assert.NoError(storage.CreateDevice(ts.DB(), &ts.Device))
-}
-
-func (ts *ClassBTestSuite) SetupTest() {
-	ts.DatabaseTestSuiteBase.SetupTest()
-
-	assert := require.New(ts.T())
-	assert.NoError(storage.FlushDeviceQueueForDevEUI(ts.DB(), ts.Device.DevEUI))
+	ts.CreateDevice(storage.Device{
+		DevEUI: lorawan.EUI64{1, 2, 3, 4, 5, 6, 7, 8},
+	})
 }
 
 func (ts *ClassBTestSuite) TestUplink() {
@@ -241,18 +215,14 @@ func (ts *ClassBTestSuite) TestUplink() {
 func (ts *ClassBTestSuite) TestDownlink() {
 	assert := require.New(ts.T())
 
-	ds := storage.DeviceSession{
-		RoutingProfileID: ts.Device.RoutingProfileID,
-		ServiceProfileID: ts.Device.ServiceProfileID,
-		DeviceProfileID:  ts.Device.DeviceProfileID,
-		DevEUI:           ts.Device.DevEUI,
-		DevAddr:          lorawan.DevAddr{1, 2, 3, 4},
-		JoinEUI:          lorawan.EUI64{8, 7, 6, 5, 4, 3, 2, 1},
-		FNwkSIntKey:      lorawan.AES128Key{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-		SNwkSIntKey:      lorawan.AES128Key{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-		NwkSEncKey:       lorawan.AES128Key{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
-		FCntUp:           8,
-		NFCntDown:        5,
+	ts.CreateDeviceSession(storage.DeviceSession{
+		DevAddr:     lorawan.DevAddr{1, 2, 3, 4},
+		JoinEUI:     lorawan.EUI64{8, 7, 6, 5, 4, 3, 2, 1},
+		FNwkSIntKey: lorawan.AES128Key{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+		SNwkSIntKey: lorawan.AES128Key{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+		NwkSEncKey:  lorawan.AES128Key{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16},
+		FCntUp:      8,
+		NFCntDown:   5,
 		UplinkGatewayHistory: map[lorawan.EUI64]storage.UplinkGatewayHistory{
 			lorawan.EUI64{1, 2, 1, 2, 1, 2, 1, 2}: storage.UplinkGatewayHistory{},
 		},
@@ -261,15 +231,15 @@ func (ts *ClassBTestSuite) TestDownlink() {
 		PingSlotFrequency:     868300000,
 		PingSlotDR:            2,
 		RX2Frequency:          869525000,
-	}
+	})
 
 	emitTime := gps.Time(time.Now().Add(-10 * time.Second)).TimeSinceGPSEpoch()
 	fPortTen := uint8(10)
 
 	txInfo := gw.DownlinkTXInfo{
 		GatewayId:         []byte{1, 2, 1, 2, 1, 2, 1, 2},
-		Frequency:         uint32(ds.PingSlotFrequency),
-		Power:             int32(config.C.NetworkServer.Band.Band.GetDownlinkTXPower(ds.PingSlotFrequency)),
+		Frequency:         uint32(ts.DeviceSession.PingSlotFrequency),
+		Power:             int32(config.C.NetworkServer.Band.Band.GetDownlinkTXPower(ts.DeviceSession.PingSlotFrequency)),
 		TimeSinceGpsEpoch: ptypes.DurationProto(emitTime),
 	}
 	assert.NoError(helpers.SetDownlinkTXInfoDataRate(&txInfo, 2, config.C.NetworkServer.Band.Band))
@@ -277,165 +247,128 @@ func (ts *ClassBTestSuite) TestDownlink() {
 	txInfoDefaultFreq := txInfo
 	txInfoDefaultFreq.Frequency = 869525000
 
-	testTable := []struct {
-		BeforeFunc       func(ds *storage.DeviceSession)
-		Name             string
-		DeviceSession    storage.DeviceSession
-		DeviceQueueItems []storage.DeviceQueueItem
-
-		ExpectedFCntUp     uint32
-		ExpectedFCntDown   uint32
-		ExpectedTXInfo     *gw.DownlinkTXInfo
-		ExpectedPHYPayload *lorawan.PHYPayload
-	}{
+	tests := []DownlinkTest{
 		{
 			Name:          "class-b downlink",
-			DeviceSession: ds,
+			DeviceSession: *ts.DeviceSession,
 			DeviceQueueItems: []storage.DeviceQueueItem{
 				{DevEUI: ts.Device.DevEUI, FPort: 10, FCnt: 5, FRMPayload: []byte{1, 2, 3}, EmitAtTimeSinceGPSEpoch: &emitTime},
 			},
-			ExpectedFCntUp:   8,
-			ExpectedFCntDown: 6,
-			ExpectedTXInfo:   &txInfo,
-			ExpectedPHYPayload: &lorawan.PHYPayload{
-				MHDR: lorawan.MHDR{
-					MType: lorawan.UnconfirmedDataDown,
-					Major: lorawan.LoRaWANR1,
-				},
-				MIC: lorawan.MIC{164, 6, 172, 129},
-				MACPayload: &lorawan.MACPayload{
-					FHDR: lorawan.FHDR{
-						DevAddr: ds.DevAddr,
-						FCnt:    5,
-						FCtrl: lorawan.FCtrl{
-							ADR: true,
+			Assert: []Assertion{
+				AssertFCntUp(8),
+				AssertNFCntDown(6),
+				AssertDownlinkFrame(txInfo, lorawan.PHYPayload{
+					MHDR: lorawan.MHDR{
+						MType: lorawan.UnconfirmedDataDown,
+						Major: lorawan.LoRaWANR1,
+					},
+					MIC: lorawan.MIC{164, 6, 172, 129},
+					MACPayload: &lorawan.MACPayload{
+						FHDR: lorawan.FHDR{
+							DevAddr: ts.DeviceSession.DevAddr,
+							FCnt:    5,
+							FCtrl: lorawan.FCtrl{
+								ADR: true,
+							},
+						},
+						FPort: &fPortTen,
+						FRMPayload: []lorawan.Payload{
+							&lorawan.DataPayload{Bytes: []byte{1, 2, 3}},
 						},
 					},
-					FPort: &fPortTen,
-					FRMPayload: []lorawan.Payload{
-						&lorawan.DataPayload{Bytes: []byte{1, 2, 3}},
-					},
-				},
+				}),
 			},
 		},
 		{
 			Name:          "class-b downlink with more data",
-			DeviceSession: ds,
+			DeviceSession: *ts.DeviceSession,
 			DeviceQueueItems: []storage.DeviceQueueItem{
 				{DevEUI: ts.Device.DevEUI, FPort: 10, FCnt: 5, FRMPayload: []byte{1, 2, 3}, EmitAtTimeSinceGPSEpoch: &emitTime},
 				{DevEUI: ts.Device.DevEUI, FPort: 10, FCnt: 6, FRMPayload: []byte{1, 2, 3}, EmitAtTimeSinceGPSEpoch: &emitTime},
 			},
-			ExpectedFCntUp:   8,
-			ExpectedFCntDown: 6,
-			ExpectedTXInfo:   &txInfo,
-			ExpectedPHYPayload: &lorawan.PHYPayload{
-				MHDR: lorawan.MHDR{
-					MType: lorawan.UnconfirmedDataDown,
-					Major: lorawan.LoRaWANR1,
-				},
-				MIC: lorawan.MIC{39, 244, 225, 101},
-				MACPayload: &lorawan.MACPayload{
-					FHDR: lorawan.FHDR{
-						DevAddr: ds.DevAddr,
-						FCnt:    5,
-						FCtrl: lorawan.FCtrl{
-							ADR:      true,
-							FPending: true,
-							ClassB:   true, // shares the same bit as FPending
+			Assert: []Assertion{
+				AssertFCntUp(8),
+				AssertNFCntDown(6),
+				AssertDownlinkFrame(txInfo, lorawan.PHYPayload{
+					MHDR: lorawan.MHDR{
+						MType: lorawan.UnconfirmedDataDown,
+						Major: lorawan.LoRaWANR1,
+					},
+					MIC: lorawan.MIC{39, 244, 225, 101},
+					MACPayload: &lorawan.MACPayload{
+						FHDR: lorawan.FHDR{
+							DevAddr: ts.DeviceSession.DevAddr,
+							FCnt:    5,
+							FCtrl: lorawan.FCtrl{
+								ADR:      true,
+								FPending: true,
+								ClassB:   true, // shares the same bit as FPending
+							},
+						},
+						FPort: &fPortTen,
+						FRMPayload: []lorawan.Payload{
+							&lorawan.DataPayload{Bytes: []byte{1, 2, 3}},
 						},
 					},
-					FPort: &fPortTen,
-					FRMPayload: []lorawan.Payload{
-						&lorawan.DataPayload{Bytes: []byte{1, 2, 3}},
-					},
-				},
+				}),
 			},
 		},
 		{
-			BeforeFunc: func(ds *storage.DeviceSession) {
-				ds.BeaconLocked = false
+			BeforeFunc: func(tst *DownlinkTest) error {
+				tst.DeviceSession.BeaconLocked = false
+				return nil
 			},
 			Name:          "class-b downlink, but no beacon lock",
-			DeviceSession: ds,
+			DeviceSession: *ts.DeviceSession,
 			DeviceQueueItems: []storage.DeviceQueueItem{
 				{DevEUI: ts.Device.DevEUI, FPort: 10, FCnt: 5, FRMPayload: []byte{1, 2, 3}, EmitAtTimeSinceGPSEpoch: &emitTime},
 			},
-			ExpectedFCntUp:   8,
-			ExpectedFCntDown: 5,
+			Assert: []Assertion{
+				AssertFCntUp(8),
+				AssertNFCntDown(5),
+			},
 		},
 		{
-			BeforeFunc: func(ds *storage.DeviceSession) {
+			BeforeFunc: func(tst *DownlinkTest) error {
 				config.C.NetworkServer.NetworkSettings.ClassB.PingSlotFrequency = 0
-				ds.PingSlotFrequency = 0
+				tst.DeviceSession.PingSlotFrequency = 0
+				return nil
 			},
 			Name:          "class-b downlink, with default band frequency plan",
-			DeviceSession: ds,
+			DeviceSession: *ts.DeviceSession,
 			DeviceQueueItems: []storage.DeviceQueueItem{
-				{DevEUI: ds.DevEUI, FPort: 10, FCnt: 5, FRMPayload: []byte{1, 2, 3}, EmitAtTimeSinceGPSEpoch: &emitTime},
+				{DevEUI: ts.Device.DevEUI, FPort: 10, FCnt: 5, FRMPayload: []byte{1, 2, 3}, EmitAtTimeSinceGPSEpoch: &emitTime},
 			},
-			ExpectedFCntUp:   8,
-			ExpectedFCntDown: 6,
-			ExpectedTXInfo:   &txInfoDefaultFreq,
-			ExpectedPHYPayload: &lorawan.PHYPayload{
-				MHDR: lorawan.MHDR{
-					MType: lorawan.UnconfirmedDataDown,
-					Major: lorawan.LoRaWANR1,
-				},
-				MIC: lorawan.MIC{164, 6, 172, 129},
-				MACPayload: &lorawan.MACPayload{
-					FHDR: lorawan.FHDR{
-						DevAddr: ds.DevAddr,
-						FCnt:    5,
-						FCtrl: lorawan.FCtrl{
-							ADR: true,
+			Assert: []Assertion{
+				AssertFCntUp(8),
+				AssertNFCntDown(6),
+				AssertDownlinkFrame(txInfoDefaultFreq, lorawan.PHYPayload{
+					MHDR: lorawan.MHDR{
+						MType: lorawan.UnconfirmedDataDown,
+						Major: lorawan.LoRaWANR1,
+					},
+					MIC: lorawan.MIC{164, 6, 172, 129},
+					MACPayload: &lorawan.MACPayload{
+						FHDR: lorawan.FHDR{
+							DevAddr: ts.DeviceSession.DevAddr,
+							FCnt:    5,
+							FCtrl: lorawan.FCtrl{
+								ADR: true,
+							},
+						},
+						FPort: &fPortTen,
+						FRMPayload: []lorawan.Payload{
+							&lorawan.DataPayload{Bytes: []byte{1, 2, 3}},
 						},
 					},
-					FPort: &fPortTen,
-					FRMPayload: []lorawan.Payload{
-						&lorawan.DataPayload{Bytes: []byte{1, 2, 3}},
-					},
-				},
+				}),
 			},
 		},
 	}
 
-	for _, tst := range testTable {
+	for _, tst := range tests {
 		ts.T().Run(tst.Name, func(t *testing.T) {
-			assert := require.New(t)
-			assert.NoError(storage.FlushDeviceQueueForDevEUI(ts.DB(), tst.DeviceSession.DevEUI))
-
-			if tst.BeforeFunc != nil {
-				tst.BeforeFunc(&tst.DeviceSession)
-			}
-			assert.NoError(storage.SaveDeviceSession(ts.RedisPool(), tst.DeviceSession))
-			for _, qi := range tst.DeviceQueueItems {
-				assert.NoError(storage.CreateDeviceQueueItem(ts.DB(), &qi))
-			}
-
-			assert.NoError(downlink.ScheduleDeviceQueueBatch(1))
-
-			ds, err := storage.GetDeviceSession(ts.RedisPool(), tst.DeviceSession.DevEUI)
-			assert.NoError(err)
-			assert.Equal(tst.ExpectedFCntUp, ds.FCntUp)
-			assert.Equal(tst.ExpectedFCntDown, ds.NFCntDown)
-
-			if tst.ExpectedTXInfo != nil && tst.ExpectedPHYPayload != nil {
-				testGW := config.C.NetworkServer.Gateway.Backend.Backend.(*test.GatewayBackend)
-				downlinkFrame := <-testGW.TXPacketChan
-				assert.NotEqual(0, downlinkFrame.Token)
-
-				downlinkFrame.TxInfo.TimeSinceGpsEpoch.XXX_sizecache = 0
-				modInfo := downlinkFrame.TxInfo.GetLoraModulationInfo()
-				modInfo.XXX_sizecache = 0
-				downlinkFrame.TxInfo.XXX_sizecache = 0
-
-				assert.Equal(tst.ExpectedTXInfo, downlinkFrame.TxInfo)
-
-				var phy lorawan.PHYPayload
-				assert.NoError(phy.UnmarshalBinary(downlinkFrame.PhyPayload))
-
-				assert.Equal(tst.ExpectedPHYPayload, &phy)
-			}
+			ts.AssertDownlinkTest(t, tst)
 		})
 	}
 }
